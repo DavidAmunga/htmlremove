5a67b0b4b2c28fae14cfac68b4e2f1d60604f3e1
David Amunga <david.onyimbo@gmail.com>
2020-03-02 16:30:46 +0300

    clean up

--- index.js
+++ index.js
@@ -11,8 +11,5 @@ var htmlRemove = (htmlString) => {
   return text;
 }
 
-// const html = htmlRemove("<div> May the force be with <a href=\"#\">you</a>. I love <strong>Batman</strong>")
-
-// console.log(html);
 
 module.exports = htmlRemove;
