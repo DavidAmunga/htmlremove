0f32308f13a3afd68e9ec7516643b8956aa42871
David Amunga <david.onyimbo@gmail.com>
2020-03-13 14:23:26 +0300

    Replaced with regex function

--- index.ts
+++ index.ts
@@ -3,19 +3,11 @@
  * @param {htmlString}
  */
 const htmlRemove = (htmlString: string): string => {
-  let div = window.document.createElement("div");
-  if (div == null) {
-    return "";
-  }
-
-  let text = "";
-  div.innerHTML = htmlString;
-  text = div.textContent!.trim();
-  text = text.replace(/\</g, "&lt;").replace(/\>/g, "&gt;");
-  return text;
+  const newText = htmlString.replace(/(<\?[a-z]*(\s[^>]*)?\?(>|$)|<!\[[a-z]*\[|\]\]>|<!DOCTYPE[^>]*?(>|$)|<!--[\s\S]*?(-->|$)|<[a-z?!\/]([a-z0-9_:.])*(\s[^>]*)?(>|$))/gi, '');
+  return newText;
 
 }
 
-htmlRemove("<p>May the force be with you <a href=\"undefined\" class=\"wysiwyg-mention\" data-mention data-value=\"tenant.firstName\">@tenant.firstName</a>  And Awesomeness</p>")
+htmlRemove("<p>May the <asdas>asdasda </asdas> force be with you <a href=\"undefined\" class=\"wysiwyg-mention\" data-mention data-value=\"tenant.firstName\">@tenant.firstName</a>  And Awesomeness</p>")
 
 export default htmlRemove;
